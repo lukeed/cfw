e85216672fe018154cb588f8d2aa7802b8d1d979
Luke Edwards <luke.edwards05@gmail.com>
2019-08-04 15:35:14 -0700

    feat(build): initial pass

---
+++ bin.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const sade = require('sade');
+const build = require('./lib/build');
+const { version } = require('./package');
+const deploy = require('./lib/deploy');
+
+sade('cfw')
+	.version(version)
+	.option('--cwd', 'The relative working directory', '.')
+	.option('--dir', 'The directory Worker scripts', 'workers')
+
+	.command('build [src] [output]')
+	.describe('Compile the Worker(s) within a directory.')
+	.option('--only', 'The list of Worker names to build; overrides `--ignore` list!')
+	.option('--ignore', 'The list of Worker names to skip')
+	.action(build)
+
+	.command('deploy [output]')
+	.describe('Deploy the built Worker(s) – requires you `build` first.')
+	.option('--only', 'The list of Worker names to build; overrides `--ignore` list!')
+	.option('--ignore', 'The list of Worker names to skip')
+	.action(deploy)
+
+	.parse(process.argv);
