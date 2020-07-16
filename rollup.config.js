f7b49cba14366d8953fd92e6520d63616352968c
Luke Edwards <luke.edwards05@gmail.com>
2020-07-16 16:17:22 -0700

    chore: install & setup `typescript` manually

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,49 @@
-const pkg = require('./package.json');
+const fs = require('fs');
+const path = require('path');
 const { minify } = require('terser');
+const { transpileModule } = require('typescript');
+const tsconfig = require('./tsconfig.json');
+const pkg = require('./package.json');
+
+const resolve = {
+	name: 'resolve',
+	resolveId(file, importer) {
+		let tmp, { ext } = path.parse(file);
+		if (ext) return file;
+
+		let next = path.resolve(path.dirname(importer), file);
+		if (fs.existsSync(next)) return next;
+
+		if (fs.existsSync(tmp = next + '.ts')) return tmp;
+		if (fs.existsSync(tmp = next + '.js')) return tmp;
+
+		return null;
+	}
+};
+
+const terser = {
+	name: 'terser',
+	renderChunk(code, _chunk, opts) {
+		return minify(code, {
+			toplevel: true,
+			sourceMap: !!opts.sourcemap,
+			compress: true,
+		});
+	}
+};
+
+const typescript = {
+	name: 'typescript',
+	transform(code, file) {
+		if (!/\.ts$/.test(file)) return code;
+		// @ts-ignore
+		let output = transpileModule(code, { ...tsconfig, fileName: file });
+		return {
+			code: output.outputText,
+			map: output.sourceMapText || null
+		};
+	}
+};
 
 module.exports = {
 	input: 'src/index.js',
@@ -15,15 +59,8 @@ module.exports = {
 		...Object.keys(pkg.dependencies),
 	],
 	plugins: [
-		{
-			name: 'terser',
-			renderChunk(code, _chunk, opts) {
-				return minify(code, {
-					toplevel: true,
-					sourceMap: !!opts.sourcemap,
-					compress: true,
-				});
-			}
-		}
+		resolve,
+		typescript,
+		terser,
 	]
 }
