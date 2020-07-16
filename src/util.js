f371972ba4dc896f2a8a85f86582823c8d6f5db6
Luke Edwards <luke.edwards05@gmail.com>
2020-07-16 15:23:01 -0700

    chore: begin jsdocs

--- src/util.js
+++ src/util.js
@@ -7,34 +7,76 @@ import { error } from './log';
 export const write = promisify(fs.writeFile);
 export const read = promisify(fs.readFile);
 
+/**
+ * @param {any} mix
+ * @param {string} msg
+ * @param {boolean} [toExist]
+ */
 export function assert(mix, msg, toExist) {
 	(toExist ? fs.existsSync(mix) : !!mix) || error(msg);
 }
 
+/**
+ * @param {Array|string} str
+ * @returns {string[]}
+ */
 export function list(str) {
 	return Array.isArray(str) ? str : str.split(',');
 }
 
+/**
+ * @param {string} str
+ * @param {string} [dir]
+ * @returns {import('module').Module|false}
+ */
 export function load(str, dir) {
 	str = resolve(dir || '.', str);
 	return fs.existsSync(str) && require(str);
 }
 
+/**
+ * @param {string} dir
+ * @param {any} [tmp]
+ * @returns {Config|void}
+ */
 export function toConfig(dir, tmp) {
 	if (tmp = load('cfw.js', dir)) return tmp;
 	if (tmp = load('cfw.json', dir)) return tmp;
 	if (tmp = load('package.json', dir)) return tmp.cfw;
 }
 
+/**
+ * @typedef Config
+ * @property {string} [name] Customize the Worker's name.
+ * @property {string} [zoneid] Customize the Worker's name.
+ * @property {string} [profile] Name of user profile to use.
+ * @property {string[]} [routes] List of route patterns to attach.
+ * @property {string} [entry] Customize entrypoint (default "index.js")
+ */
+
+/**
+ * @typedef WorkerData
+ * @property {string} input Entry point.
+ * @property {string} name Name of the worker script.
+ * @property {string} abs Absolute path to worker directory.
+ * @property {Config} [cfw] Custom `cfw` options
+ */
+
+/**
+ * @param {string} dir
+ * @param {string} name
+ * @param {boolean} [isOne]
+ * @returns {WorkerData}
+ */
 export function toWorker(dir, name, isOne) {
 	let abs = isOne ? dir : join(dir, name);
-
-	let out = { abs, name };
 	let config = toConfig(abs) || {};
-	out.input = join(abs, config.entry || 'index.js');
-	if (config.name) out.name = config.name;
-	out.cfw = config;
-	return out;
+	return {
+		cfw: config,
+		name: config.name || name,
+		input: join(abs, config.entry || 'index.js'),
+		abs,
+	};
 }
 
 export function toWorkers(dir, opts) {
@@ -70,6 +112,20 @@ export function toWorkers(dir, opts) {
 	return arr;
 }
 
+/**
+ * @typedef Profile
+ * @property {string} cloudflare_auth_key
+ * @property {string} cloudflare_auth_email
+ * @property {string} cloudflare_accountid
+ * @property {string} [cloudflare_zoneid]
+ */
+
+/**
+ * Parse `~/.cfw/config` file.
+ * Load a custom credentials `profile`
+ * @param {string} [profile]
+ * @returns {Promise<Profile>}
+ */
 async function toProfile(profile = 'default') {
 	let file = join(homedir(), '.cfw', 'config');
 	assert(file, `Missing "${file}" config file`, true);
@@ -96,6 +152,19 @@ async function toProfile(profile = 'default') {
 	return out;
 }
 
+/**
+ * @typedef Credentials
+ * @property {string} zoneid Target Zone ID
+ * @property {string} accountid Target Account ID
+ * @property {string} [token] An API token; required if `authkey` and `email` are missing.
+ * @property {string} [authkey] Account authorization key; required if `token` is missing.
+ * @property {string} [email] Account email address; required if `token` is missing.
+ */
+
+/**
+ * @param {Config} def
+ * @returns {Promise<Credentials>}
+ */
 export async function toCredentials(def) {
 	let {
 		CLOUDFLARE_ZONEID, CLOUDFLARE_ACCOUNTID,
