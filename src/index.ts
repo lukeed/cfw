5d3f00ffd4fb137f7269547aa20dc6e3ace6e4a7
Luke Edwards <luke.edwards05@gmail.com>
2020-07-17 16:02:18 -0700

    chore: define `bin` commands

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as build } from './commands/build';
 export { default as deploy } from './commands/deploy';
+export * as secret from './commands/secrets';
 export * as ns from './commands/names';
