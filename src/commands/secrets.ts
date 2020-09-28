0b63246af58c9272c52f551cf3b73bb330aa8970
Luke Edwards <luke.edwards05@gmail.com>
2020-09-28 12:46:43 -0700

    feat: add `secrets create` action

--- src/commands/secrets.ts
+++ src/commands/secrets.ts
@@ -34,12 +34,27 @@ export async function create(key: string, value: string, opts: Partial<Options>)
 	let items = utils.toWorkers(opts.dir, opts as Options);
 	if (!items.length) return log.missing('No workers found!', opts);
 
+	let arrow = colors.cyan(log.ARROW);
+	let count = colors.bold(items.length);
+	let sfx = items.length === 1 ? '' : 's';
+	let toAdd: Array<() => Promise<void>> = [];
+	log.info(`Adding secret "${key}" value to ${count} worker${sfx}:`);
+
 	for (let def of items) {
 		let { name, cfw } = def;
 		cfw.profile = cfw.profile || opts.profile;
 		let creds = await utils.toCredentials(cfw);
-		console.log('~> name', name, { key, value }, creds);
+		toAdd.push(() => {
+			let now = Date.now();
+			return Secrets.create(creds, name, key, value).then(res => {
+				if (res.success) console.log(arrow + name + log.time(Date.now() - now));
+			})
+		});
 	}
+
+	await Promise.all(toAdd.map(x => x()));
+
+	log.success(`Added secret to worker${sfx}`);
 }
 
 export async function destroy(key: string, opts: Partial<Options>) {
