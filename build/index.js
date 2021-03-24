7c94ff82338f8e9b566b132647ba8680cd01a0d2
Luke Edwards <luke.edwards05@gmail.com>
2021-03-23 18:10:05 -0700

    chore: use `esbuild` to self-bundle

---
+++ build/index.js
@@ -0,0 +1,23 @@
+const { build } = require('esbuild');
+const pkg = require('../package.json');
+
+(async function () {
+	await build({
+		bundle: true,
+		format: 'esm',
+		entryPoints: [
+			'src/index.ts',
+		],
+		outfile: 'lib/index.mjs',
+		sourcemap: false,
+		charset: 'utf8',
+		// minify: true,
+		external: [
+			...require('module').builtinModules,
+			...Object.keys(pkg.dependencies),
+		]
+	})
+})().catch(err => {
+	console.error(err.stack);
+	process.exitCode = 1;
+});
